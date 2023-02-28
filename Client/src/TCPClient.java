30998e748b58ff5450f4b45dd812a4d1e31eddde
wndudwkd003 <ymail3@naver.com>
2023-03-01 02:38:51 +0900

    서버 수정

--- Client/src/TCPClient.java
+++ Client/src/TCPClient.java
@@ -1,99 +1,129 @@
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 import static java.lang.Thread.sleep;
 
 public class TCPClient {
-    private static boolean isClientRun = false;
+    private static boolean isRunning;
     private static Socket clientSocket = null;
-    public static void main(String[] args) throws Exception {
+    private static final int SERVER_PORT = 12345;
+    public static void main(String[] args) {
         String serverHostname = "127.0.0.1"; // 서버 IP 주소
-        int serverPort = 12345; // 서버 포트번호
-
-        // 서버에 연결
-
+        isRunning = true;
+        // 서버에 연결하는 쓰레드
+        // 서버에 연결 중인 클라이언트는 한 번만 연결하면 해당 쓰레드는 필요 없지만
+        // 서버가 끊기고 다시 서버에 자동을 접속하려면 루프가 필요하다
         Thread serverThread = new Thread(() -> {
-            while (!isClientRun) {
-                try {
-                    if (clientSocket == null) {
+            // 서로 다른 쓰레드에서 동작한다
+            ServerToClientThread serverToClientThread = null;    // 서버에서 클라이언트로 메시지를 받는 쓰레드
+            ClientToServerThread clientToServerThread = null;    // 클라이언트에서 서버로 메시지를 보내는 쓰레드
+
+            while (isRunning) {
+                if (clientSocket == null) {
+                    try {
                         System.out.println("Server finding...");
-                        try {
-                            clientSocket = new Socket(serverHostname, serverPort);
+                        clientSocket = new Socket(serverHostname, SERVER_PORT);
+                        if (clientSocket.isConnected()) {
                             System.out.println("Connected to server.");
-                            if (clientSocket.isConnected()) {
-                                isClientRun = true;
-                                InputStream in = clientSocket.getInputStream();
-                                BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-
-                                OutputStream out = clientSocket.getOutputStream();
-                                BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-
-
-
-                                Thread serverToClientThread = new Thread(() -> {
-                                    // 서버로부터 응답 수신
-                                    while (true) {
-                                        try {
-                                            String response = reader.readLine();
-                                            if (response == null) {
-                                                // 서버와의 연결이 끊어진 경우
-                                                System.out.println("Disconnected from server.");
-                                                isClientRun = false;
-                                                clientSocket = null;
-                                                break;
-                                            }
-                                            System.out.println("Response from server: " + response);
-                                        } catch (IOException e) {
-                                            System.out.println("Disconnected from server.");
-                                            try {
-                                                br.close();
-                                                reader.close();
-                                            } catch (IOException ex) {
-                                                throw new RuntimeException(ex);
-                                            }
-
-                                            clientSocket = null;
-                                            break;
-                                        }
-                                    }
-                                });
-
-
-
-                                Thread clientToServerThread = new Thread(() -> {
-                                    // 서버로 메시지 전송
-                                    while (true) {
-                                        try {
-                                            String input = br.readLine();
-                                            if(input.equals("exit")) {
-                                                isClientRun = false;
-                                                break;
-                                            }
-                                            PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8), true);
-                                            writer.println(input);
-                                        } catch (IOException e) {
-                                            System.out.println("Disconnected from server.");
-                                            break;
-                                        }
-                                    }
-                                });
-
-                                clientToServerThread.start();
-                                serverToClientThread.start();
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                            // 서버와 접속하면 소켓을 사용하는 쓰레드 생성
+                            serverToClientThread = new ServerToClientThread();
+                            serverToClientThread.start();
+                            clientToServerThread = new ClientToServerThread();
+                            clientToServerThread.start();
                         }
-
-                        sleep(1000);
+                    } catch (IOException e) {
+                        System.out.println("Failed to connect to server. Retrying...");
+                        // 서버와 연결이 끊기면 현재 접속중인 소켓을 사용중인 쓰레드를 전부 종료한다
+                        if (serverToClientThread != null) {
+                            serverToClientThread.setFlag(false);
+                        }
+                        if (clientToServerThread!= null) {
+                            clientToServerThread.setFlag(false);
+                        }
                     }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                }
+                try {
+                    // 재접속은 1초 간격으로 한다
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {
                 }
             }
         });
         serverThread.start();
 
     }
+
+    static class ClientToServerThread extends Thread {
+        private boolean flag = true;
+        private final OutputStream out;
+        private final BufferedReader br;
+
+        ClientToServerThread() throws IOException {
+            out = clientSocket.getOutputStream();
+            br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            // 서버로 메시지 전송
+            while (flag) {
+                try {
+                    // 서버로 보낼 메시지를 받는 코드
+                    // 애플리케이션 자체를 종료하려면 해당 명령어 입력
+                    String input = br.readLine();
+                    if(input.equals("exit")) {
+                        System.out.println("Client Off... Goodbye!");
+                        clientSocket = null;
+                        isRunning = false;
+                        break;
+                    }
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8), true);
+                    writer.println(input);
+                } catch (IOException e) {
+                    System.out.println("Failed to send message.");
+                }
+            }
+        }
+
+        public void setFlag(boolean flag) {
+            this.flag = flag;
+        }
+    }
+
+    static class ServerToClientThread extends Thread {
+        private boolean flag = true;
+        private final BufferedReader reader;
+
+        public ServerToClientThread() throws IOException {
+            InputStream in = clientSocket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            // 서버로부터 응답 수신
+            while (flag) {
+                try {
+                    String response = reader.readLine();
+                    System.out.println("Response from server: " + response);
+                } catch (IOException e) {
+                    // 서버와 연결이 끊기면 현재 사용중인 소켓은 null 으로 변경하여
+                    // 서버 접속 쓰레드에서 재접속 하도록 유도
+                    System.out.println("Disconnected from server.");
+                    clientSocket = null;
+                    break;
+                }
+            }
+        }
+
+        public void setFlag(boolean flag) {
+            this.flag = flag;
+        }
+    }
+
+
 }
--- Server/src/TCPServer.java
+++ Server/src/TCPServer.java
@@ -3,65 +3,81 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
-public class TCPServer {
+import static java.lang.System.exit;
 
-    private static final Socket[] clientSockets = new Socket[51];
+public class TCPServer {
+    private static final int SERVER_PORT = 12345; // 서버 포트번호
+    private static final int SERVER_SPACE = 50; // 서버에서 가용할 수 있는 클라이언트 수 (임의 변경)
+    private static final Socket[] clientSockets = new Socket[SERVER_SPACE];    // 클라이언트 소켓 배열
+    private static ServerSocket serverSocket;   // 서버 소켓
 
     public static void main(String[] args) throws Exception {
 
-        int serverPort = 12345; // 서버 포트번호
-
         // 서버 소켓 생성
-        ServerSocket serverSocket = new ServerSocket(serverPort);
+        serverSocket = new ServerSocket(SERVER_PORT);
         System.out.println("TCP Server started.");
 
+        // 서버 오픈
+        // 서버는 항상 루프가 돌고 있어야 하기 때문에 쓰레드로 구현
+        // 나머지도 마찬가지
         Thread serverAcceptThread = new Thread(() -> {
+            int i = 0;
             while (true) {
-                for (int i = 0; i < clientSockets.length; i++) {
-                    try {
-                        // 클라이언트 대기
-                        System.out.println("Waiting for clients...");
-                        clientSockets[i] = serverSocket.accept();
-                        System.out.println("Client connected: " + clientSockets[i].getInetAddress().getHostAddress());
+                try {
+                    // 클라이언트 대기
+                    System.out.println("Waiting for clients...");
+                    // 클라이언트가 접속할때가지 아래 코드에서 기다린다
+                    // 클라이언트가 접속하지 않으면 코드가 더이상 진행되지 않는다
+                    clientSockets[i] = serverSocket.accept();
+                    System.out.println("Client connected: " + clientSockets[i].getInetAddress().getHostAddress());
 
-                        Socket clientSocket = clientSockets[i];
-
-                        if (clientSocket != null && clientSocket.isConnected()) {
-                            InputStream in = clientSocket.getInputStream();
-                            BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                            Thread clientToServerThread = new Thread(() -> {
-                                // 클라이언트로부터 메시지 수신
-                                while (true) {
-                                    try {
-                                        String message = reader.readLine();
-                                        System.out.println("Message from client: " + message);
-
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                            clientToServerThread.start();
-                        }
-
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                    if (clientSockets[i] != null && clientSockets[i].isConnected()) {
+                        // 클라이언트에서 서버로 메시지를 전송받는 클래스 생성
+                        ClientToServerThread clientHandler = new ClientToServerThread(i);
+                        clientHandler.start();
                     }
+
+                    // 클라이언트 최대 수 만큼 인덱스가 자동 증가하고 넘어가는 숫자는 0부터 시작
+                    // 이렇게 하면 클라이언트가 서버의 남는 소켓이 없도록 차곡차곡 접속할 수 있다
+                    i = (i + 1) % clientSockets.length;
+                } catch (IOException e) {
+                    serverSocket = null;
+                    break;
                 }
             }
 
         });
         serverAcceptThread.start();
 
-        Thread serverInputThread = new Thread(() -> {
+        // 서버에서 클라이언트로 메시지를 보내는 클래스 생성
+        ServerToClientThread serverToClientThread = new ServerToClientThread();
+        serverToClientThread.start();
+    }
+
+    static class ServerToClientThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
             try {
                 while(true) {
                     BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
                     String input = br.readLine();
 
+                    // 서버를 종료하고 싶을 때
+                    if(input.equals("exit")) {
+                        System.out.println("Server off... Goodbye!");
+                        for (Socket socket : clientSockets) {
+                            if (socket!=null) {
+                                socket.close();
+                            }
+                        }
+                        serverSocket.close();
+                        break;
+                    }
+
+                    // 소켓 배열의 접속되어 있는 클라이언트를 찾아서 차례대로 전송한다
                     for (Socket clientSocket : clientSockets) {
                         if (clientSocket!= null && clientSocket.isConnected()) {
-                            System.out.println("Sending message to client: " + input);
                             OutputStream out = clientSocket.getOutputStream();
                             PrintWriter writer = new PrintWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8), true);
                             writer.println(input);
@@ -69,9 +85,36 @@ public class TCPServer {
                     }
                 }
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
-        });
-        serverInputThread.start();
+        }
+    }
+
+    static class ClientToServerThread extends Thread {
+        private final int index;
+        private final InputStream in;
+        private final BufferedReader reader;
+
+        ClientToServerThread(int index) throws IOException {
+            this.index = index;
+            this.in = clientSockets[index].getInputStream();
+            this.reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            while (true) {
+                // 클라이언트로부터 메시지 수신
+                try {
+                    String message = reader.readLine();
+                    System.out.println("Message from client: " + message);
+                } catch (IOException e) {
+                    System.out.printf("Client[%d] disconnected.\n", index);
+                    clientSockets[index] = null;
+                    break;
+                }
+            }
+        }
     }
 }
